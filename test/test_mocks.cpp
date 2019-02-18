fb0049d712b48993c1c33f5b7c1c45135cee60cf
Konstantin Belyalov <arsenicus@mail.ru>
2019-02-18 15:32:36 -0800

    Implement SPI/I2C unittest mocks

--- test/test_mocks.cpp
+++ test/test_mocks.cpp
@@ -1,28 +1,111 @@
 // Copyright (c) Konstantin Belyalov. All rights reserved.
 // Licensed under the MIT license.
 
+#include <string>
+#include <deque>
+
 #include "main.h"
+#include "test_mocks.h"
+
+using namespace std;
+
+static deque<string> spi_transmit_history;
+static deque<string> spi_transmit_queue;
+
+static deque<string> i2c_transmit_history;
+static deque<string> i2c_transmit_queue;
+
+
+// SPI mock interface: check history / schedule data to be received
+string SPI_get_transmit_history_entry(size_t index)
+{
+  return spi_transmit_history[index];
+}
+
+void SPI_queue_receive_data(const string& data)
+{
+  spi_transmit_queue.push_back(data);
+}
+
+void SPI_clear_transmit_history()
+{
+  spi_transmit_history.clear();
+}
+
+void SPI_clear_transmit_queue()
+{
+  spi_transmit_queue.clear();
+}
 
 // SPI mocks
 EXPORT HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout)
 {
+  spi_transmit_history.push_back(string((const char*)pData, Size));
+
   return HAL_OK;
 }
 
 EXPORT HAL_StatusTypeDef HAL_SPI_Receive(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout)
 {
+  assert(spi_transmit_queue.size() > 0);
+
+  string& data = spi_transmit_queue.front();
+  if (data.size() != Size) {
+    printf("SPI wants %d bytes, but %lu scheduled\n", Size, data.size());
+    assert(data.size() == Size);
+  }
+
+  memcpy(pData, data.data(), Size);
+  spi_transmit_queue.pop_front();
+
   return HAL_OK;
 }
 
+
+// I2C mock interface: check history / schedule data to be received
+string I2C_get_transmit_history_entry(size_t index)
+{
+  return i2c_transmit_history[index];
+}
+
+void I2C_queue_receive_data(const string& data)
+{
+  i2c_transmit_queue.push_back(data);
+}
+
+void I2C_clear_transmit_history()
+{
+  i2c_transmit_history.clear();
+}
+
+void I2C_clear_transmit_queue()
+{
+  i2c_transmit_queue.clear();
+}
+
 // I2C mocks
+
 EXPORT HAL_StatusTypeDef HAL_I2C_Master_Transmit(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
 {
-    return HAL_OK;
+  i2c_transmit_history.push_back(string((const char*)pData, Size));
+
+  return HAL_OK;
 }
 
 EXPORT HAL_StatusTypeDef HAL_I2C_Master_Receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
 {
-    return HAL_OK;
+  assert(i2c_transmit_queue.size() > 0);
+
+  string& data = i2c_transmit_queue.front();
+  if (data.size() != Size) {
+    printf("I2C wants %d bytes, but %lu scheduled\n", Size, data.size());
+    assert(data.size() == Size);
+  }
+
+  memcpy(pData, data.data(), Size);
+  i2c_transmit_queue.pop_front();
+
+  return HAL_OK;
 }
 
 // GPIO mocks
---
+++ test/test_mocks.h
@@ -0,0 +1,18 @@
+// Copyright (c) Konstantin Belyalov. All rights reserved.
+// Licensed under the MIT license.
+
+#ifndef __TEST_MOCKS__H
+#define __TEST_MOCKS__H
+
+std::string SPI_get_transmit_history_entry(size_t index);
+void        SPI_queue_receive_data(const std::string& data);
+void        SPI_clear_transmit_history();
+void        SPI_clear_transmit_queue();
+
+// I2C mock interface: check history / schedule data to be received
+std::string I2C_get_transmit_history_entry(size_t index);
+void        I2C_queue_receive_data(const std::string& data);
+void        I2C_clear_transmit_history();
+void        I2C_clear_transmit_queue();
+
+#endif
