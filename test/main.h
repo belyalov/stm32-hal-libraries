f142ca0a2bd08f674db4ba0cf9d0014ad1c747dd
Konstantin Belyalov <arsenicus@mail.ru>
2019-02-18 12:42:13 -0800

    Add unittest support - by gtest. Also compile ARM binary, not just objects

--- test/main.h
+++ test/main.h
@@ -1,7 +1,14 @@
 // Copyright (c) Konstantin Belyalov. All rights reserved.
 // Licensed under the MIT license.
+
 #ifndef __MAIN_H
 #define __MAIN_H
+
+#ifdef __cplusplus
+#define EXPORT extern "C"
+#else
+#define EXPORT
+#endif
 
 #include <stdint.h>
 
@@ -32,23 +39,25 @@ typedef struct
 {
 } GPIO_TypeDef;
 
+typedef struct
+{
+} I2C_HandleTypeDef;
+
+
 // HAL function mocks
 
-HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout)
-{
-  return HAL_OK;
-}
-HAL_StatusTypeDef HAL_SPI_Receive(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout)
-{
-  return HAL_OK;
-}
+// SPI mocks
+EXPORT HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+EXPORT HAL_StatusTypeDef HAL_SPI_Receive(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout);
 
-void HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
-{
-}
+// I2C mocks
+EXPORT HAL_StatusTypeDef HAL_I2C_Master_Transmit(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+EXPORT HAL_StatusTypeDef HAL_I2C_Master_Receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout);
 
-void HAL_Delay(uint32_t Delay)
-{
-}
+// GPIO mocks
+EXPORT void HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
+
+// Misc
+EXPORT void HAL_Delay(uint32_t Delay);
 
 #endif
---
+++ test/main_arm.c
@@ -0,0 +1,42 @@
+// Copyright (c) Konstantin Belyalov. All rights reserved.
+// Licensed under the MIT license.
+
+#include "main.h"
+
+// SPI mocks
+HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+  return HAL_OK;
+}
+
+HAL_StatusTypeDef HAL_SPI_Receive(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+  return HAL_OK;
+}
+
+// I2C mocks
+HAL_StatusTypeDef HAL_I2C_Master_Transmit(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef HAL_I2C_Master_Receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+    return HAL_OK;
+}
+
+// GPIO mocks
+void HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
+{
+}
+
+// Misc
+void HAL_Delay(uint32_t Delay)
+{
+}
+
+int main()
+{
+    return 0;
+}
+
