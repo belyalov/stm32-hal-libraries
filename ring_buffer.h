71d8cee4666a3e9021e066467faf70528d05b8bd
Konstantin Belyalov <arsenicus@mail.ru>
2019-04-07 22:23:08 -0700

    Add Ring Buffer

---
+++ ring_buffer.h
@@ -0,0 +1,57 @@
+// Copyright (c) Konstantin Belyalov. All rights reserved.
+// Licensed under the MIT license.
+
+#ifndef __RING_BUFFER_H
+#define __RING_BUFFER_H
+
+// Defines and initializes ring buffer structure.
+// Params:
+//  - _name: variable name to be created
+//  - _item: ring buffer item type
+//  - _size: ring size (items count)
+#define RING_BUFFER_DEFINE_INIT(_name, _item, _size)  \
+    struct _name##_ring {                             \
+        size_t head;                                  \
+        size_t tail;                                  \
+        size_t size;                                  \
+        uint8_t full;                                 \
+        _item buffer[_size];                          \
+    };                                                \
+    struct _name##_ring _name = {0, 0, _size, 0};
+
+// Pushes "_item" into ring buffer (does memcpy)
+#define RING_BUFFER_PUSH_COPY(_name, _item)                                 \
+    ({                                                                      \
+        (_name)->head = ((_name)->head + 1) % (_name)->size;                \
+        memcpy(&(_name)->buffer[(_name)->head], _item, sizeof(*_item));     \
+        if (!(_name)->full) (_name)->full = (_name)->head == (_name)->tail; \
+    })
+
+// "Pushes" item (basically does pointer advance) and returns it.
+// You must fill copy data yourself
+#define RING_BUFFER_PUSH(_name)                                               \
+    ({                                                                        \
+        (_name)->head = ((_name)->head + 1) % (_name)->size;                  \
+        if (!(_name)->full) (_name)->full = ((_name)->head == (_name)->tail); \
+        &(_name)->buffer[(_name)->head];   \
+    })
+
+// POPs item from ring buffer (NOTE: it does not check for empty queue)
+// and returns pointer to the item
+#define RING_BUFFER_POP(_name)                                      \
+    ({                                                              \
+        (_name)->tail = ((_name)->tail + 1) % (_name)->size;        \
+        (_name)->full = 0;                                          \
+        &(_name)->buffer[(_name)->tail];                            \
+    })
+
+// Returns pointer to current tail
+#define RING_BUFFER_TAIL(_name) (&(_name)->buffer[((_name)->tail + 1) % (_name)->size])
+
+// Returns True if buffer is full
+#define RING_BUFFER_FULL(_name) ((_name)->full)
+
+// Returns True if buffer is empty
+#define RING_BUFFER_EMPTY(_name) (!(_name)->full && (_name)->head == (_name)->tail)
+
+#endif
---
+++ test/test_ring.cpp
@@ -0,0 +1,97 @@
+// Copyright (c) Konstantin Belyalov. All rights reserved.
+// Licensed under the MIT license.
+
+#include <gtest/gtest.h>
+
+#include "ring_buffer.h"
+
+using namespace std;
+
+struct item {
+  uint32_t a, b;
+};
+
+
+TEST(ring_buffer, push_pop_simple)
+{
+  // Create ring buffer of 10 items
+  RING_BUFFER_DEFINE_INIT(rbuf, struct item, 10);
+  // Ensure that it is empty :)
+  ASSERT_TRUE(RING_BUFFER_EMPTY(&rbuf));
+
+  // Push item using PUSH_COPY
+  struct item item1 = {33, 44};
+  RING_BUFFER_PUSH_COPY(&rbuf, &item1);
+  ASSERT_FALSE(RING_BUFFER_EMPTY(&rbuf));
+  ASSERT_FALSE(RING_BUFFER_FULL(&rbuf));
+
+  // Pop item
+  struct item *item2 = RING_BUFFER_TAIL(&rbuf);
+  ASSERT_EQ(33, item2->a);
+  ASSERT_EQ(44, item2->b);
+  // RING_BUFFER_TAIL does not move pointer
+  ASSERT_FALSE(RING_BUFFER_EMPTY(&rbuf));
+  ASSERT_FALSE(RING_BUFFER_FULL(&rbuf));
+  struct item *item3 = RING_BUFFER_POP(&rbuf);
+  ASSERT_EQ(item2, item3);
+  ASSERT_TRUE(RING_BUFFER_EMPTY(&rbuf));
+  ASSERT_FALSE(RING_BUFFER_FULL(&rbuf));
+}
+
+TEST(ring_buffer, push_pop_all)
+{
+  // Create ring buffer of 10 items
+  RING_BUFFER_DEFINE_INIT(rbuf, struct item, 10);
+  // Ensure that it is empty :)
+  ASSERT_TRUE(RING_BUFFER_EMPTY(&rbuf));
+
+  // Add item 10 items
+  for (uint32_t i = 0; i < 9; i++) {
+    struct item i_pushed = {i * 10, i * 100};
+    RING_BUFFER_PUSH_COPY(&rbuf, &i_pushed);
+    ASSERT_FALSE(RING_BUFFER_FULL(&rbuf));
+    ASSERT_FALSE(RING_BUFFER_EMPTY(&rbuf));
+  }
+  // Edge item
+  {
+    struct item *pushed = RING_BUFFER_PUSH(&rbuf);
+    ASSERT_TRUE(RING_BUFFER_FULL(&rbuf));
+    ASSERT_FALSE(RING_BUFFER_EMPTY(&rbuf));
+    pushed->a = 90;
+    pushed->b = 900;
+  }
+  // Pop all 10 items
+  for (uint32_t i = 0; i < 10; i++) {
+    ASSERT_FALSE(RING_BUFFER_EMPTY(&rbuf));
+    struct item *i_pop = RING_BUFFER_POP(&rbuf);
+    ASSERT_EQ(i * 10, i_pop->a);
+    ASSERT_EQ(i * 100, i_pop->b);
+    ASSERT_FALSE(RING_BUFFER_FULL(&rbuf));
+  }
+  ASSERT_TRUE(RING_BUFFER_EMPTY(&rbuf));
+}
+
+TEST(ring_buffer, push_override)
+{
+  // Create ring buffer of 10 items
+  RING_BUFFER_DEFINE_INIT(rbuf, struct item, 10);
+
+  // Add item 20 items (first 10 must be overridden)
+  for (uint32_t i = 0; i < 20; i++) {
+    struct item i_pushed = {i * 10, i * 100};
+    RING_BUFFER_PUSH_COPY(&rbuf, &i_pushed);
+    if (i >= 9) {
+      ASSERT_TRUE(RING_BUFFER_FULL(&rbuf));
+    } else {
+      ASSERT_FALSE(RING_BUFFER_FULL(&rbuf));
+    }
+  }
+
+  // Pop all items (expect only last 10)
+  for (uint32_t i = 0; i < 10; i++) {
+    struct item *i_pop = RING_BUFFER_POP(&rbuf);
+    ASSERT_EQ((i + 10) * 10, i_pop->a);
+    ASSERT_EQ((i + 10) * 100, i_pop->b);
+    ASSERT_FALSE(RING_BUFFER_FULL(&rbuf));
+  }
+}
