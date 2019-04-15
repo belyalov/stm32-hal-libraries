e2121f742ad9c4f883772b834d3eadf3af277101
Konstantin Belyalov <arsenicus@mail.ru>
2019-04-14 17:27:55 -0700

    [utils] Add htons / htonl macros with tests

---
+++ test/test_utils.cpp
@@ -0,0 +1,48 @@
+// Copyright (c) Konstantin Belyalov. All rights reserved.
+// Licensed under the MIT license.
+
+#include <gtest/gtest.h>
+
+// Under several systems macros HTON* are already defined.
+// Undef them before.
+#undef HTONS
+#undef NTOHS
+#undef HTONL
+#undef NTOHL
+#include "utils.h"
+
+using namespace std;
+
+TEST(utils, htons)
+{
+  map<uint16_t, uint16_t> runs = {
+    {0, 0},
+    {1, 0x0100},
+    {0x0100, 1},
+    {0x2233, 0x3322},
+    {0xFFFF, 0xFFFF},
+  };
+
+  for (auto it = runs.begin(); it != runs.end(); ++it) {
+    uint16_t res = HTONS(it->first);
+    // Check result
+    ASSERT_EQ(it->second, res);
+  }
+}
+
+TEST(utils, htonl)
+{
+  map<uint32_t, uint32_t> runs = {
+    {0, 0},
+    {1, 0x01000000},
+    {0x01000000, 1},
+    {0x22334455, 0x55443322},
+    {0xFFFFFFFF, 0xFFFFFFFF},
+  };
+
+  for (auto it = runs.begin(); it != runs.end(); ++it) {
+    uint32_t res = HTONL(it->first);
+    // Check result
+    ASSERT_EQ(it->second, res);
+  }
+}
---
+++ utils.h
@@ -0,0 +1,17 @@
+// Copyright (c) Konstantin Belyalov. All rights reserved.
+// Licensed under the MIT license.
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#define HTONS(x) ( ((x) << 8) | ( ((x) >> 8) & 0xFF) )
+#define NTOHS(x) HTONS(x)
+
+#define HTONL(x) ( ((x) << 24 & 0xFF000000UL) |         \
+                   ((x) <<  8 & 0x00FF0000UL) |         \
+                   ((x) >>  8 & 0x0000FF00UL) |         \
+                   ((x) >> 24 & 0x000000FFUL) )
+#define NTOHL(x) HTONL(x)
+
+#endif
+
